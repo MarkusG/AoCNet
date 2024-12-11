9d4bce6a9628a53f92be4037f29c71406ef4cd1b
Mark Gross <mark.gross2001@gmail.com>
2024-12-10 23:12:02 -0800

    11: use p2 impl for p1
    and this makes both benchmarks way slower

--- AoCNet/2024/Day11.cs
+++ AoCNet/2024/Day11.cs
@@ -4,58 +4,21 @@ namespace AoC._2024;
 
 public class Day11 : AdventBase
 {
-    protected override object InternalPart1()
+    private static ulong Stones(ulong n, int maxBlinks, Dictionary<(ulong, int), ulong> cache, int blink = 0)
     {
-        var stones = Input.Text.Split(' ').Select(ulong.Parse).ToList();
-        const int blinks = 25;
-        for (var i = 0; i < blinks; i++)
-        {
-            for (var j = 0; j < stones.Count; j++)
-            {
-                var stone = stones[j];
-                if (stone == 0)
-                {
-                    stones[j] = 1;
-                    continue;
-                }
-
-                var digits = (int)Math.Log10(stone) + 1;
-                if (digits % 2 == 0)
-                {
-                    var stoneString = stone.ToString();
-
-                    var stoneOne = ulong.Parse(stoneString[..(digits / 2)]);
-                    var stoneTwo = ulong.Parse(stoneString[(digits / 2)..]);
-
-                    stones[j] = stoneTwo;
-                    stones.Insert(j++, stoneOne);
-                    continue;
-                }
-
-                stones[j] = stone * 2024;
-            }
-        }
-
-        return stones.Count;
-    }
-
-    private readonly Dictionary<(ulong, int), ulong> _stoneCounts = new();
-
-    private ulong Stones(ulong n, int maxBlinks, int blink = 0)
-    {
-        if (_stoneCounts.TryGetValue((n, blink), out var count))
+        if (cache.TryGetValue((n, blink), out var count))
             return count;
 
         if (blink >= maxBlinks)
         {
-            _stoneCounts.TryAdd((n, blink), 1);
+            cache.TryAdd((n, blink), 1);
             return 1;
         }
 
         if (n == 0)
         {
-            var stones = Stones(1, maxBlinks, blink + 1);
-            _stoneCounts.TryAdd((n, blink), stones);
+            var stones = Stones(1, maxBlinks, cache, blink + 1);
+            cache.TryAdd((n, blink), stones);
             return stones;
         }
 
@@ -67,25 +30,33 @@ public class Day11 : AdventBase
             var stoneOne = ulong.Parse(stoneString[..(digits / 2)]);
             var stoneTwo = ulong.Parse(stoneString[(digits / 2)..]);
 
-            var stones = Stones(stoneOne, maxBlinks, blink + 1) + Stones(stoneTwo, maxBlinks, blink + 1);
-            _stoneCounts.TryAdd((n, blink), stones);
+            var stones = Stones(stoneOne, maxBlinks, cache, blink + 1) + Stones(stoneTwo, maxBlinks, cache, blink + 1);
+            cache.TryAdd((n, blink), stones);
             return stones;
         }
 
-        var stones2 = Stones(n * 2024, maxBlinks, blink + 1);
-        _stoneCounts.TryAdd((n, blink), stones2);
+        var stones2 = Stones(n * 2024, maxBlinks, cache, blink + 1);
+        cache.TryAdd((n, blink), stones2);
         return stones2;
+    }
+
+    protected override object InternalPart1()
+    {
+        var stones = Input.Text.Split(' ').Select(ulong.Parse).ToList();
+
+        var cache = new Dictionary<(ulong, int), ulong>();
+
+        return stones.Select(s => Stones(s, 25, cache))
+            .Aggregate<ulong, ulong>(0, (current, n) => current + n);
     }
 
     protected override object InternalPart2()
     {
         var stones = Input.Text.Split(' ').Select(ulong.Parse).ToList();
-        const int blinks = 75;
 
-        ulong count = 0;
-        foreach (var n in stones.Select(s => Stones(s, blinks)))
-            count += n;
-
-        return count;
+        var cache = new Dictionary<(ulong, int), ulong>();
+        
+        return stones.Select(s => Stones(s, 75, cache))
+            .Aggregate<ulong, ulong>(0, (current, n) => current + n);
     }
 }
