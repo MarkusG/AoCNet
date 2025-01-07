c1f2f307a2a70e8537691b1215b630953d781127
Mark Gross <mark.gross2001@gmail.com>
2025-01-06 23:58:41 -0800

    01: 01p1 optimization

--- AoCNet/2024/Day01.cs
+++ AoCNet/2024/Day01.cs
@@ -1,27 +1,82 @@
-﻿using AdventOfCodeSupport;
+﻿using System.Numerics;
+using System.Runtime.Intrinsics;
+using AdventOfCodeSupport;
 
 namespace AoC._2024;
 
 public class Day01 : AdventBase
 {
+    private static int[] _left = null!;
+    private static int[] _right = null!;
+
+    protected override void InternalOnLoad()
+    {
+        _left = new int[1000];
+        _right = new int[1000];
+
+        var blockIdx = 0;
+        var blockSize = Vector256<int>.Count;
+
+        do
+        {
+            var left = Vector256<int>.Zero;
+            for (var i = 0; i < 5; i++)
+            {
+                left *= 10;
+                var next = Vector256.Create(
+                    Input.Bytes[blockIdx * blockSize * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 1) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 2) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 3) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 4) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 5) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 6) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 7) * 14 + i] - '0'
+                );
+                left += next;
+            }
+
+            var right = Vector256<int>.Zero;
+
+            for (var i = 8; i < 13; i++)
+            {
+                right *= 10;
+                var next = Vector256.Create(
+                    Input.Bytes[blockIdx * blockSize * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 1) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 2) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 3) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 4) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 5) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 6) * 14 + i] - '0',
+                    Input.Bytes[(blockIdx * blockSize + 7) * 14 + i] - '0'
+                );
+                right += next;
+            }
+
+            left.CopyTo(_left, blockIdx * blockSize);
+            right.CopyTo(_right, blockIdx * blockSize);
+        } while (++blockIdx * blockSize < 1000);
+
+        Array.Sort(_left);
+        Array.Sort(_right);
+    }
+
     protected override object InternalPart1()
     {
-        var lines = Input.Lines;
-        var left = new List<int>();
-        var right = new List<int>();
-        foreach (var l in lines)
-        {
-            left.Add(int.Parse(l[..5]));
-            right.Add(int.Parse(l[7..]));
-        }
-        
-        var totalDistance = left
-            .Order()
-            .Zip(right.Order())
-            .Select(x => Math.Abs(x.First - x.Second))
-            .Sum();
+        var result = Vector<int>.Zero;
+        var index = 0;
 
-        return totalDistance;
+        do
+        {
+            var left = new Vector<int>(_left, index);
+            var right = new Vector<int>(_right, index);
+            result += Vector.Abs(left - right);
+
+            index += Vector<int>.Count;
+        } while (index < _left.Length);
+
+        return Vector.Sum(result);
     }
 
     protected override object InternalPart2()
@@ -34,7 +89,7 @@ public class Day01 : AdventBase
             left.Add(int.Parse(l[..5]));
             right.Add(int.Parse(l[7..]));
         }
-        
+
         var occurrences = new Dictionary<int, int>();
         foreach (var x in right)
         {
--- AoCNet/Program.cs
+++ AoCNet/Program.cs
@@ -1,7 +1,7 @@
 using AdventOfCodeSupport;
 
 var solutions = new AdventSolutions();
-var today = solutions.GetMostRecentDay();
+var today = solutions.GetDay(2024, 1);
 
 today.Part1().Part2();
 today.Benchmark();
